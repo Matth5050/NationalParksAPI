1f649ee120e1a0ac89d0a1f0a3b19eb025fc35c8
Matth5050 <matthew.f.herbert@gmail.com>
2022-08-19 14:41:13 -0700

    update login route for authmanagement

--- NationalParks.Solution/Controllers/AuthManagementController.cs
+++ NationalParks.Solution/Controllers/AuthManagementController.cs
@@ -106,4 +106,52 @@ public class AuthManagementController : ControllerBase
 
         return jwtToken;
     }
+    [HttpPost]
+    [Route("Login")]
+    public async Task<IActionResult> Login([FromBody] UserLoginRequest user)
+    {
+        if(ModelState.IsValid)
+        {
+            // check if the user with the same email exist
+            var existingUser = await _userManager.FindByEmailAsync(user.Email);
+
+            if(existingUser == null) 
+            {
+                // We dont want to give to much information on why the request has failed for security reasons
+                return BadRequest(new RegistrationResponse() {
+                                        Result = false,
+                                        Errors = new List<string>(){
+                                            "Invalid authentication request"
+                                        }});
+            }
+
+            // Now we need to check if the user has inputed the right password
+            var isCorrect = await _userManager.CheckPasswordAsync(existingUser, user.Password);
+
+            if(isCorrect)
+            {
+                var jwtToken = GenerateJwtToken(existingUser);
+
+                return Ok(new RegistrationResponse() {
+                        Result = true, 
+                        Token = jwtToken
+                });
+            }
+            else 
+            {
+                // We dont want to give to much information on why the request has failed for security reasons
+                return BadRequest(new RegistrationResponse() {
+                                        Result = false,
+                                        Errors = new List<string>(){
+                                            "Invalid authentication request"
+                                        }});
+            }
+        }
+
+        return BadRequest(new RegistrationResponse() {
+                                        Result = false,
+                                        Errors = new List<string>(){
+                                            "Invalid payload"
+                                        }});
+    }
 }
