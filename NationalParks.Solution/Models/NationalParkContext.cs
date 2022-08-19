56a75df63b28a3dc98f0f3666b3a05e76205839a
Matth5050 <matthew.f.herbert@gmail.com>
2022-08-19 10:48:56 -0700

    updated new databases

--- NationalParks.Solution/Models/NationalParkContext.cs
+++ NationalParks.Solution/Models/NationalParkContext.cs
@@ -19,7 +19,13 @@ namespace NationalPark.Models
                 new Park { ParkId = 4, Name = "Everglades", State = "Florida" },
                 new Park { ParkId = 5, Name = "Arches", State = "Colorado" }
             );
+
+        builder.Entity<Region>()
+        .HasData(
+            new Region{ RegionId = 1, Name = "West" }
+            );
         }
         public DbSet<Park> Parks { get; set; }
+        public DbSet<Region> Regions { get; set; }
     }
 }
