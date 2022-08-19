ad67b1ed395ee1c6986bda7156719be9e7f7e169
Matth5050 <matthew.f.herbert@gmail.com>
2022-08-19 10:03:25 -0700

    added neew CRUD functionality

--- NationalParks.Solution/Controllers/ParksController.cs
+++ NationalParks.Solution/Controllers/ParksController.cs
@@ -1,8 +1,11 @@
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using NationalPark.Models;
+using System;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 
 namespace NationalPark.Controllers
 {
@@ -15,6 +18,20 @@ namespace NationalPark.Controllers
     public ParksController(NationalParkContext db)
     {
       _db = db;
+    }
+
+    // GET: api/Animals/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Park>> GetPark(int id)
+    {
+      var park = await _db.Parks.FindAsync(id);
+
+      if (park == null)
+      {
+          return NotFound();
+      }
+
+      return park;
     }
 
     // GET api/parks
@@ -31,7 +48,58 @@ namespace NationalPark.Controllers
       _db.Parks.Add(park);
       await _db.SaveChangesAsync();
 
-      return CreatedAtAction("Post", new { id = park.ParkId }, park);
+      return CreatedAtAction(nameof(GetPark), new { id = park.ParkId }, park);
+    }
+
+    // PUT: api/parks/5
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, Park park)
+    {
+      if (id != park.ParkId)
+      {
+        return BadRequest();
+      }
+
+      _db.Entry(park).State = EntityState.Modified;
+
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!ParkExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+
+      return NoContent();
+    }
+
+    // DELETE: api/parks/5
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeletePark(int id)
+    {
+      var park = await _db.Parks.FindAsync(id);
+      if (park == null)
+      {
+        return NotFound();
+      }
+
+      _db.Parks.Remove(park);
+      await _db.SaveChangesAsync();
+
+      return NoContent();
+    }
+    private bool ParkExists(int id)
+    {
+      return _db.Parks.Any(e => e.ParkId == id);
     }
   }
 }
+  
