76d3db1c52ac0e735ed9c5cac10283fed200d551
Matth5050 <matthew.f.herbert@gmail.com>
2022-08-19 10:48:15 -0700

    edited park model

--- NationalParks.Solution/Models/Park.cs
+++ NationalParks.Solution/Models/Park.cs
@@ -1,9 +1,15 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace NationalPark.Models
 {
     public class Park
     {
         public int ParkId { get; set; }
+        public int RegionId { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string State { get; set; }
     }
 }
