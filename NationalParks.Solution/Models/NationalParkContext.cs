6c54975a2c20e0e91615d74a21dd2138a5530141
Matth5050 <matthew.f.herbert@gmail.com>
2022-08-19 10:36:10 -0700

    add seeddata

--- NationalParks.Solution/Models/NationalParkContext.cs
+++ NationalParks.Solution/Models/NationalParkContext.cs
@@ -9,6 +9,17 @@ namespace NationalPark.Models
         {
         }
 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+        builder.Entity<Park>()
+            .HasData(
+                new Park { ParkId = 1, Name = "Zion", State = "Utah" },
+                new Park { ParkId = 2, Name = "YellowStone", State = "Wyoming" },
+                new Park { ParkId = 3, Name = "Sequoia", State = "California" },
+                new Park { ParkId = 4, Name = "Everglades", State = "Florida" },
+                new Park { ParkId = 5, Name = "Arches", State = "Colorado" }
+            );
+        }
         public DbSet<Park> Parks { get; set; }
     }
 }
