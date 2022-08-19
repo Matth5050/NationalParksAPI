6a601a4b6cf8eeef7d2532cab4fafa913eac8de0
Matth5050 <matthew.f.herbert@gmail.com>
2022-08-19 10:35:18 -0700

    Add seeddata migrations

---
+++ NationalParks.Solution/Migrations/20220819171011_SeedData.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NationalParks.Solution.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Parks",
+                columns: new[] { "ParkId", "Name", "State" },
+                values: new object[,]
+                {
+                    { 1, "Zion", "Utah" },
+                    { 2, "YellowStone", "Wyoming" },
+                    { 3, "Sequoia", "California" },
+                    { 4, "Everglades", "Florida" },
+                    { 5, "Arches", "Colorado" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 5);
+        }
+    }
+}
