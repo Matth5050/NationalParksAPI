e57e448a5092270e2efc255aba2a6def203e5b75
Matth5050 <matthew.f.herbert@gmail.com>
2022-08-21 16:32:22 -0700

    created apiHelper

--- NationalParksClient.Solution/Models/ApiHelper.cs
+++ NationalParksClient.Solution/Models/ApiHelper.cs
@@ -12,5 +12,42 @@ namespace NationalParkClient.Models
       var response = await client.ExecuteTaskAsync(request);
       return response.Content;
     }
+
+    public static async Task<string> Get(string requestType, int id)
+    {
+      RestClient client = new RestClient("http://localhost:5000/api");
+      RestRequest request = new RestRequest($"{requestType}/{id}", Method.GET);
+      var response = await client.ExecuteTaskAsync(request);
+      return response.Content;
+    }
+
+    public static async Task Post(string requestType, string newPost)
+    {
+      RestClient client = new RestClient("http://localhost:5000/api");
+      RestRequest request = new RestRequest($"{requestType}", Method.POST);
+      request.AddHeader("Content-Type", "application/json");
+      request.AddHeader("Authorization", "Bearer " + AppUser.Token);
+      request.AddJsonBody(newPost);
+      var response = await client.ExecuteTaskAsync(request);
+    }
+
+    public static async Task Put(string requestType, int id, string newPut)
+    {
+      RestClient client = new RestClient("http://localhost:5000/api");
+      RestRequest request = new RestRequest($"{requestType}/{id}", Method.PUT);
+      request.AddHeader("Content-Type", "application/json");
+      request.AddHeader("Authorization", "Bearer " + AppUser.Token);
+      request.AddJsonBody(newPut);
+      var response = await client.ExecuteTaskAsync(request);
+    }
+
+    public static async Task Delete(string requestType, int id)
+    {
+      RestClient client = new RestClient("http://localhost:5000/api");
+      RestRequest request = new RestRequest($"{requestType}/{id}", Method.DELETE);
+      request.AddHeader("Content-Type", "application/json");
+      request.AddHeader("Authorization", "Bearer " + AppUser.Token);
+      var response = await client.ExecuteTaskAsync(request);
+    }
   }
 }
